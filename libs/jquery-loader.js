44d6762b6f6f790d1b1c3d0ae5fbaf638c933fae
Matt Bower <matt@webbower.com>
2012-11-10 18:55:03 -0800

    - jQuery loader loads jQuery UI now - Replaced jQuery Core and Widget components with upgraded custom build - Updated index.html with redone assets

--- libs/jquery-loader.js
+++ libs/jquery-loader.js
@@ -1,14 +1,24 @@
 (function() {
   // Get any jquery=___ param from the query string.
   var jqversion = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
-  var path;
+  var jquiversion = location.search.match(/[?&]jqueryui=(.*?)(?=&|$)/);
+  var jqpath;
+  var jquipath;
   if (jqversion) {
     // A version was specified, load that version from code.jquery.com.
-    path = 'http://code.jquery.com/jquery-' + jqversion[1] + '.js';
+    jqpath = 'http://code.jquery.com/jquery-' + jqversion[1] + '.js';
   } else {
     // No version was specified, load the local version.
-    path = '../libs/jquery/jquery.js';
+    jqpath = '../libs/jquery/jquery.js';
+  }
+  if (jquiversion) {
+    // A version was specified, load that version from code.jquery.com.
+    jquipath = 'http://code.jquery.com/ui/' + jquiversion[1] + '/jquery-ui.js';
+  } else {
+    // No version was specified, load the local version.
+    jquipath = '../libs/jqueryui/jquery-ui.js';
   }
   // This is the only time I'll ever use document.write, I promise!
-  document.write('<script src="' + path + '"></script>');
+  document.write('<script src="' + jqpath + '"></script>');
+  document.write('<script src="' + jquipath + '"></script>');
 }());
